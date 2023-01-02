3d0a5b9bcb54a26b510cc0f35e6aa27d156f34b5
James <1jamesthompson1@gmail.com>
2023-01-03 12:30:50 +1300

    Make readme only update when it needs to.

--- R/dev_workflow.r
+++ R/dev_workflow.r
@@ -6,7 +6,10 @@
 .renderAndStyle <- function(location = ".") {
   styler::style_pkg(location)
   devtools::document(location)
-  if (location == ".") {
+
+  # the readme can only be built when in the base package location
+  # It will also only run if the readme file is newer.
+  if (location == "." && file.info("README.Rmd")$mtime > file.info("README.Rmd")$mtime) {
     devtools::build_readme(location)
   }
 }
